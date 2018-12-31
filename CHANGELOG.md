fe8329ef7ab26b9281b8ebd3dac7d6c8bcbfd191
et4891 <et1103@hotmail.com>
2018-12-30 17:41:34 -0800

    v0.3.0 able to delete dir from s3 when dir is removed, which will also remove all files within

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@
 ## ChangeLog
 
 ### Dec. 30, 2018
+**v0.3.0**
+- able to delete dir from s3 when dir is removed, which will also remove all files within
+
 **v0.2.0**
 - able to delete from s3 when file is removed
 
--- watcher/index.js
+++ watcher/index.js
@@ -4,9 +4,9 @@ const AWS = require('aws-sdk');
 
 
 const { s3Put } = require('./s3Put');
-// // const { s3PutDir } = require('./s3PutDir');
+// const { s3PutDir } = require('./s3PutDir');
 const { s3Delete } = require('./s3Delete');
-// const { s3DeleteDir } = require('./s3DeleteDir');
+const { s3DeleteDir } = require('./s3DeleteDir');
 
 module.exports = {
     log: console.log.bind(console),
@@ -31,9 +31,9 @@ module.exports = {
         watcher
             .on('add', filePath => s3Put(filePath, watchPath, s3, s3Config))
             .on('unlink', filePath => s3Delete(filePath, watchPath, s3, s3Config))
-            // .on('addDir', path => (`Directory ${path} has been added`))
-            // .on('unlinkDir', path => s3DeleteDir(path))
-            // .on('error', error => this.log(`Watcher error: ${error}`))
+            .on('addDir', path => (`Directory ${path} has been added`))
+            .on('unlinkDir', filePath => s3DeleteDir(filePath, watchPath, s3, s3Config))
+            .on('error', error => this.log(`Watcher error: ${error}`))
             .on('ready', () => this.log('Initial scan complete. Ready for changes'));
         // .on('raw', (event, path, details) => {
         //     this.log('Raw event info:', event, path, details);
