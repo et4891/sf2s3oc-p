01c93a2f60793df38d87c669e941049550524c09
et4891 <et1103@hotmail.com>
2019-01-06 16:14:25 -0800

    comment

--- utils/consts/regex.js
+++ utils/consts/regex.js
@@ -1,4 +1,5 @@
 // using string as value because we might do multiple regex check such as using IGNORE_FILES_TO_S3
+// can be using array instead of object which is less coding but rather use object because the value can be a description
 const REGEXS = {
     files: {
         GOUTPUTSTREAM: '^.goutputstream.*', // any files starts with .goutputstream this is because when file is edited, this file will be created automatically and removed
